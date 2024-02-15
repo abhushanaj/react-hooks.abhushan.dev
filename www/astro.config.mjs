1354052a4deb89a60c8446266ac4197013ed31ae
Abhushan Adhikari Joshi <49617450+abhushanaj@users.noreply.github.com>
2024-02-15 12:46:40 +0530

    feat: setup prettier as code formatter on the workspace (#4)

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -5,54 +5,53 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  // just personal preference
-  server: {
-    port: 3000,
-  },
-  site: 'https://react-hooks.abhushan.dev',
-  integrations: [
-    react(),
-    // for starlight
-    starlight({
-      title: 'React Hooks',
-      social: {
-        github: 'https://github.com/abhushanaj/react-hooks.abhushan.dev',
-      },
-      logo: {
-        light: './src/assets/logo-light.svg',
-        dark: './src/assets/logo-dark.svg',
-        replacesTitle: true,
-      },
-      favicon: '/favicon.svg',
-      head: [
-        {
-          tag: 'link',
-          attrs: {
-            rel: 'icon',
-            href: '/favicon.ico',
-            sizes: '32x32',
-          },
-        },
-      ],
-      editLink: {
-        baseUrl:
-          'https://github.com/abhushanaj/react-hooks.abhushan.dev/edit/main/',
-      },
-      sidebar: [
-        // {
-        //   label: 'Overview',
-        //   items: [
-        //     // Each item here is one entry in the navigation menu.
-        //     { label: 'Example Guide', link: '/guides/example/' },
-        //   ],
-        // },
-        {
-          label: 'Overview',
-          autogenerate: {
-            directory: 'overview',
-          },
-        },
-      ],
-    }),
-  ],
+	// just personal preference
+	server: {
+		port: 3000
+	},
+	site: 'https://react-hooks.abhushan.dev',
+	integrations: [
+		react(),
+		// for starlight
+		starlight({
+			title: 'React Hooks',
+			social: {
+				github: 'https://github.com/abhushanaj/react-hooks.abhushan.dev'
+			},
+			logo: {
+				light: './src/assets/logo-light.svg',
+				dark: './src/assets/logo-dark.svg',
+				replacesTitle: true
+			},
+			favicon: '/favicon.svg',
+			head: [
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'icon',
+						href: '/favicon.ico',
+						sizes: '32x32'
+					}
+				}
+			],
+			editLink: {
+				baseUrl: 'https://github.com/abhushanaj/react-hooks.abhushan.dev/edit/main/'
+			},
+			sidebar: [
+				// {
+				//   label: 'Overview',
+				//   items: [
+				//     // Each item here is one entry in the navigation menu.
+				//     { label: 'Example Guide', link: '/guides/example/' },
+				//   ],
+				// },
+				{
+					label: 'Overview',
+					autogenerate: {
+						directory: 'overview'
+					}
+				}
+			]
+		})
+	]
 });

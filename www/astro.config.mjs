f1217ffd26980b110561cee9ae2ac6f1f98b7f11
Abhushan A. Joshi <abhushanadhikarijoshi@gmail.com>
2024-02-15 11:14:09 +0530

    feat: astro starlight docs starter installed

---
+++ www/astro.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  // just personal preference
+  server: {
+    port: 3000,
+    open: true,
+  },
+  integrations: [
+    starlight({
+      title: 'My Docs',
+      social: {
+        github: 'https://github.com/withastro/starlight',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', link: '/guides/example/' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+});

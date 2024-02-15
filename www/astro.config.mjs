e538edaebb39c46e01439b77b443b0a6570d0d77
Abhushan A. Joshi <abhushanadhikarijoshi@gmail.com>
2024-02-15 11:38:46 +0530

    chore: cleanup and add defaults for the starlight docs

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -6,25 +6,44 @@ export default defineConfig({
   // just personal preference
   server: {
     port: 3000,
-    open: true,
   },
   integrations: [
     starlight({
-      title: 'My Docs',
+      title: 'React Hooks',
       social: {
-        github: 'https://github.com/withastro/starlight',
+        github: 'https://github.com/abhushanaj/react-hooks.abhushan.dev',
+      },
+      logo: {
+        light: './src/assets/logo-light.svg',
+        dark: './src/assets/logo-dark.svg',
+        replacesTitle: true,
+      },
+      favicon: '/favicon.svg',
+      head: [
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'icon',
+            href: '/favicon.ico',
+            sizes: '32x32',
+          },
+        },
+      ],
+      editLink: {
+        baseUrl:
+          'https://github.com/abhushanaj/react-hooks.abhushan.dev/edit/main/',
       },
       sidebar: [
+        // {
+        //   label: 'Overview',
+        //   items: [
+        //     // Each item here is one entry in the navigation menu.
+        //     { label: 'Example Guide', link: '/guides/example/' },
+        //   ],
+        // },
         {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
-          ],
-        },
-        {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
+          label: 'Overview',
+          autogenerate: { directory: 'overview' },
         },
       ],
     }),

9f263a4ac01d4fc8a64a7d5afd52d72c16123db8
Abhushan A. Joshi <abhushanadhikarijoshi@gmail.com>
2024-02-16 14:32:26 +0530

    feat: add eslint on the hooks package

---
+++ react-hooks/.eslintrc.cjs
@@ -0,0 +1,56 @@
+/** @type {import('eslint').Linter.Config} */
+const config = {
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	parser: '@typescript-eslint/parser',
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended-type-checked',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:import/recommended',
+		'plugin:react/jsx-runtime',
+		'prettier'
+	],
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 'latest',
+		ecmaFeatures: {
+			jsx: true
+		},
+		project: ['./tsconfig.json'],
+		tsconfigRootDir: __dirname
+	},
+	plugins: ['@typescript-eslint', 'react'],
+	rules: {
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{
+				argsIgnorePattern: '^_',
+				varsIgnorePattern: '^_',
+				caughtErrorsIgnorePattern: '^_'
+			}
+		],
+		'no-shadow': 'off',
+		'@typescript-eslint/no-shadow': ['error'],
+		'no-console': [2, { allow: ['warn', 'error'] }]
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		},
+		'import/extensions': ['.ts', '.tsx'],
+		'import/resolver': {
+			node: {
+				extensions: ['.ts', '.tsx']
+			}
+		}
+	},
+	ignorePatterns: ['dist/**', 'node_modules/**', '.eslintrc.cjs']
+};
+
+module.exports = config;
--- react-hooks/src/index.ts
+++ react-hooks/src/index.ts
@@ -1,3 +1,4 @@
 export function helloWorld() {
+	// eslint-disable-next-line no-console
 	console.log('Hello world! This is the start of @abhushanaj/react-hooks project');
 }

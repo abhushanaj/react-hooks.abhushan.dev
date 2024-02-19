4526f4c533fe5dc07785b6f7d068b908dd4927dc
Abhushan A. Joshi <abhushanadhikarijoshi@gmail.com>
2024-02-19 16:44:44 +0530

    feat: add the JS doc comments

--- react-hooks/src/hooks/useOnline/index.ts
+++ react-hooks/src/hooks/useOnline/index.ts
@@ -1,5 +1,20 @@
 import React from 'react';
 
+/**
+ * useOnline() hook: Get the status of the network using the navigator.onLine property along with subscription for changes.
+ * @param {boolean} defaultStatus - Default status of the network for server snapshots. Defaults to true.
+ * @returns {boolean} - The status of the network, whether it is online or not.
+ * @example
+ * function App() {
+ *   const online = useOnline();
+ *   return (
+ *     <div>
+ *       <p>The application is {online ? '🟢' : '🔴'}</p>
+ *     </div>
+ *   );
+ * }
+ */
+
 export function useOnline(defaultStatus = true) {
 	const getSnapshot = React.useCallback(() => {
 		return window.navigator.onLine;

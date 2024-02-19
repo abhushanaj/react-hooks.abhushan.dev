887fb30ab5b02afb48a58cd310a226c40f5a62ab
Abhushan A. Joshi <abhushanadhikarijoshi@gmail.com>
2024-02-19 16:00:43 +0530

    feat: add js docs comments

--- react-hooks/src/hooks/useDefault/index.ts
+++ react-hooks/src/hooks/useDefault/index.ts
@@ -3,6 +3,24 @@ import React from 'react';
 type InitialValues<T> = T | null | undefined;
 type DefaultValues<T> = T extends null ? never : T extends undefined ? never : T;
 
+/**
+ * useDefault(): React hook that manages a state value with optional default value. Returns the default value whenever state becomes null or undefined
+ *
+ * @template T - The type of the state value.
+ *
+ * @param {InitialValues<T>} initialValue - The initial value for the state. Can be `null` or `undefined`.
+ * @param {DefaultValues<T>} defaultValue - The default value to use when the state is `null` or `undefined`.
+ *
+ * @throws {Error} Throws an error if the `defaultValue` is `undefined` or `null`.
+ *
+ * @returns {[T, React.Dispatch<React.SetStateAction<InitialValues<T>>>]} - A tuple containing the current state value and its setter function.
+ *
+ * @example
+ * import {useDefaultValue} from '@abhushanaj/react-hooks';
+ * function App(){
+ *   const [selectedFruit, setSelectedFruit]= React.useDefault(person, 'Apples');
+ * }
+ */
 export function useDefault<T>(initialValue: InitialValues<T>, defaultValue: DefaultValues<T>) {
 	const [value, setValue] = React.useState<InitialValues<T>>(initialValue);
 

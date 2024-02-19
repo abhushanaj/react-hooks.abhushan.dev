967a0bb47453dd34387dfa5f59371d7510735063
Abhushan A. Joshi <abhushanadhikarijoshi@gmail.com>
2024-02-19 16:19:41 +0530

    feat: add useDefaultValue hook docs

---
+++ www/src/components/demo/useDefaultValue/index.tsx
@@ -0,0 +1,53 @@
+import { useDefault } from '@abhushanaj/react-hooks';
+
+import Button from '@/components/docs/button';
+
+function UsePreviousHookExample() {
+	const [count, setCount] = useDefault(1, 0);
+
+	return (
+		<div className="w-full">
+			<div className="my-2 flex flex-col gap-2">
+				<p className="mt-0">Current Value: {count}</p>
+			</div>
+
+			<div className="mt-6 flex flex-wrap items-center justify-center gap-8">
+				<Button
+					onClick={() => {
+						setCount(count + 1);
+					}}
+				>
+					Increment by 1
+				</Button>
+
+				<Button
+					onClick={() => {
+						setCount(count - 1);
+					}}
+				>
+					Decrement by 1
+				</Button>
+			</div>
+
+			<div className="mt-6 flex flex-wrap items-center justify-center gap-8">
+				<Button
+					onClick={() => {
+						setCount(null);
+					}}
+				>
+					Set to null
+				</Button>
+
+				<Button
+					onClick={() => {
+						setCount(undefined);
+					}}
+				>
+					Set to undefined
+				</Button>
+			</div>
+		</div>
+	);
+}
+
+export default UsePreviousHookExample;

c4a566be91802a1159ef43a51c76aff5329c6052
Abhushan A. Joshi <abhushanadhikarijoshi@gmail.com>
2024-02-19 17:40:09 +0530

    feat: add js doc comment and export entry

--- react-hooks/src/hooks/useNavigatorLanguage/index.ts
+++ react-hooks/src/hooks/useNavigatorLanguage/index.ts
@@ -1,5 +1,21 @@
 import React from 'react';
 
+/**
+ * useNavigatorLanguage() - Returns the language set on the browser for a user.
+ *
+ * @param {string} [defaultLang='en-US'] - Optional default language choice to use on server snapshots.
+ * @returns {string} - A string representing the language version as defined in RFC 5646.
+ *
+ * @example
+ * function App() {
+ *     const preferredLanguage = useNavigatorLanguage();
+ *     return (
+ *         <div>
+ *             <p>Preferred language is {preferredLanguage}</p>
+ *         </div>
+ *     );
+ * }
+ */
 export function useNavigatorLanguage(defaultLang = 'en-US') {
 	const getSnapShot = React.useCallback(() => {
 		return window.navigator.language;

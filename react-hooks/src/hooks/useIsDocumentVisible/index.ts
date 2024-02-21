9b6ee2236f741ea4658202493b39b2791674736a
Abhushan A. Joshi <abhushanadhikarijoshi@gmail.com>
2024-02-21 17:25:29 +0530

    feat: add js docs comment

--- react-hooks/src/hooks/useIsDocumentVisible/index.ts
+++ react-hooks/src/hooks/useIsDocumentVisible/index.ts
@@ -1,5 +1,32 @@
 import React from 'react';
 
+/**
+ * useIsDocumentVisible() - Hook to subscribe and return the document's visibility state using the `document.visibilityState` property.
+ *
+ * @returns {boolean} Returns a boolean indicating whether the document is currently visible.
+ *
+ * @example
+ * import React from 'react';
+ * import { useCounter , useIsDocumentVisible } from '@abhushanaj/react-hooks';
+ *
+ * // Usage example to count the number of times the tab away action was done
+ * const ExampleComponent = () => {
+ *   const [tabAwayCount, { increment }] = useCounter(0);
+ *   const isVisible = useIsDocumentVisible();
+ *
+ *   React.useEffect(() => {
+ *     if (!isVisible) {
+ *       increment();
+ *     }
+ *   }, [isVisible]);
+ *
+ *   return (
+ *     <div>
+ *       <p>Tab Away Count: {tabAwayCount}</p>
+ *     </div>
+ *   );
+ * };
+ */
 export function useIsDocumentVisible() {
 	const getSnapshot = React.useCallback(() => {
 		return document.visibilityState === 'visible';

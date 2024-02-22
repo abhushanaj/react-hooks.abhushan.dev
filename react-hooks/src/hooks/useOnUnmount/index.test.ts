3a4913756c100d3e07c165eeb6a6d0c971f50a0b
Abhushan A. Joshi <abhushanadhikarijoshi@gmail.com>
2024-02-22 13:50:56 +0530

    chore: update tests with strict mode for the useOnUnmount hook

--- react-hooks/src/hooks/useOnUnmount/index.test.ts
+++ react-hooks/src/hooks/useOnUnmount/index.test.ts
@@ -1,3 +1,4 @@
+import React from 'react';
 import { renderHook } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 
@@ -9,7 +10,7 @@ describe('useOnUnmount() hook', () => {
 		expect(useOnUnmount).toBeDefined();
 	});
 
-	it('runs the callback after unmount', () => {
+	it('runs the callback on unmount', () => {
 		const mockedCb = vi.fn();
 
 		expect.hasAssertions();
@@ -44,7 +45,7 @@ describe('useOnUnmount() hook', () => {
 		expect(mockedCb).toHaveBeenCalledOnce();
 	});
 
-	it('runs only unmount for first reference calback,  on re-renders with different callback reference', () => {
+	it('runs only unmount for first reference calback, even on re-renders with different callback reference', () => {
 		const mockedCb1 = vi.fn();
 		expect.hasAssertions();
 		const { rerender, unmount } = renderHook(({ cb }) => useOnUnmount(cb), {
@@ -53,16 +54,76 @@ describe('useOnUnmount() hook', () => {
 			}
 		});
 
+		expect(mockedCb1).not.toHaveBeenCalled();
+
 		const mockedCb2 = vi.fn();
 		rerender({
 			cb: mockedCb2
 		});
-
-		expect(mockedCb1).not.toHaveBeenCalled();
 		expect(mockedCb2).not.toHaveBeenCalled();
 
 		unmount();
-		expect(mockedCb1).toHaveBeenCalledOnce();
+		expect(mockedCb1).toHaveBeenCalledTimes(1);
 		expect(mockedCb2).not.toHaveBeenCalled();
+	});
+
+	// With Strict Mode Enabled
+	describe('when strict mode is enabled', () => {
+		it('runs twice on unmount', () => {
+			const mockedCb = vi.fn();
+			expect.hasAssertions();
+			const { unmount } = renderHook(({ cb }) => useOnUnmount(cb), {
+				initialProps: {
+					cb: mockedCb
+				},
+				wrapper: React.StrictMode
+			});
+
+			unmount();
+			expect(mockedCb).toHaveBeenCalledTimes(2);
+		});
+
+		it('runs only on unmount, even on re-renders with same callback reference', () => {
+			const mockedCb = vi.fn();
+			expect.hasAssertions();
+			const { rerender, unmount } = renderHook(({ cb }) => useOnUnmount(cb), {
+				initialProps: {
+					cb: mockedCb
+				},
+				wrapper: React.StrictMode
+			});
+
+			rerender({
+				cb: mockedCb
+			});
+
+			unmount();
+			expect(mockedCb).toHaveBeenCalledTimes(2);
+		});
+
+		it('runs only unmount for first reference calback,  even on re-renders with different callback reference', () => {
+			const mockedCb1 = vi.fn();
+			expect.hasAssertions();
+			const { rerender, unmount } = renderHook(({ cb }) => useOnUnmount(cb), {
+				initialProps: {
+					cb: mockedCb1
+				},
+				wrapper: React.StrictMode
+			});
+
+			// due to double mounts
+			expect(mockedCb1).toHaveBeenCalledTimes(1);
+
+			const mockedCb2 = vi.fn();
+			rerender({
+				cb: mockedCb2
+			});
+			expect(mockedCb2).not.toHaveBeenCalled();
+
+			unmount();
+			// due to double mounts + manual unmount
+			expect(mockedCb1).toHaveBeenCalledTimes(2);
+			expect(mockedCb2).not.toHaveBeenCalled();
+		});
 	});
 });

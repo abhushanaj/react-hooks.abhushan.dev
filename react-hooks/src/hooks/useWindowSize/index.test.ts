b6310daffd5693baf4718766ed0dc7e7486594dc
Abhushan Adhikari Joshi <49617450+abhushanaj@users.noreply.github.com>
2024-02-27 19:10:11 +0530

    Fix/update use window size (#64)
    * feat: add test case for unmount
    
    * fix: typo on the docs

--- react-hooks/src/hooks/useWindowSize/index.test.ts
+++ react-hooks/src/hooks/useWindowSize/index.test.ts
@@ -7,9 +7,6 @@ describe('useWindowSize() hook', () => {
 	const windowWidthSpy = vi.spyOn(window, 'innerWidth', 'get');
 	const windowHeightSpy = vi.spyOn(window, 'innerHeight', 'get');
 
-	const defaultWindowWidth = 5_00;
-	const defaultWindowHeight = 7_00;
-
 	it('should be defined', () => {
 		expect.hasAssertions();
 		expect(useWindowSize).toBeDefined();
@@ -17,6 +14,9 @@ describe('useWindowSize() hook', () => {
 
 	it('should return the correction dimesions of the window on render', () => {
 		expect.hasAssertions();
+
+		const defaultWindowWidth = 5_00;
+		const defaultWindowHeight = 7_00;
 
 		windowWidthSpy.mockReturnValue(defaultWindowWidth);
 		windowHeightSpy.mockReturnValue(defaultWindowHeight);
@@ -30,13 +30,10 @@ describe('useWindowSize() hook', () => {
 	it('should update dimensions based on the resize event', () => {
 		expect.hasAssertions();
 
-		windowWidthSpy.mockReturnValue(defaultWindowWidth);
-		windowHeightSpy.mockReturnValue(defaultWindowHeight);
-
 		const { result } = renderHook(() => useWindowSize());
 
-		const updatedWidth = 100;
-		const updatedHeight = 200;
+		const updatedWidth = 1000;
+		const updatedHeight = 500;
 
 		// mock the resize event action
 		act(() => {
@@ -47,5 +44,30 @@ describe('useWindowSize() hook', () => {
 
 		expect(result.current.width).toBe(updatedWidth);
 		expect(result.current.height).toBe(updatedHeight);
+	});
+
+	it('should not return latest dimensions after component unmount', () => {
+		expect.hasAssertions();
+
+		const initialWidth = 1_00;
+		const initialHeight = 3_00;
+
+		windowWidthSpy.mockReturnValue(initialWidth);
+		windowHeightSpy.mockReturnValue(initialHeight);
+
+		const { result, unmount } = renderHook(() => useWindowSize());
+
+		// we unmount here, to the values of resize event shuld not reflect anymore
+		unmount();
+
+		act(() => {
+			// random values updated by resize
+			windowWidthSpy.mockReturnValue(500);
+			windowHeightSpy.mockReturnValue(600);
+			window.dispatchEvent(new Event('resize'));
+		});
+
+		expect(result.current.width).toBe(initialWidth);
+		expect(result.current.height).toBe(initialHeight);
 	});
 });

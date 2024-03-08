5fd22fd1d06ed74122cfb5bc85566af325545911
Abhushan A. Joshi <abhushanadhikarijoshi@gmail.com>
2024-03-08 16:09:24 +0530

    feat: add link for docs

--- react-hooks/src/hooks/useWindowEventListener/index.ts
+++ react-hooks/src/hooks/useWindowEventListener/index.ts
@@ -3,6 +3,10 @@ import type { UseEventListenerCallback, UseEventListenerEventMap, UseEventListen
 import { _useEventListener } from '../_useEventListener';
 import { useIsClient } from '../useIsClient';
 
+/**
+ * useWindowEventListener() - Custom react hook to attch event listeners to window object.
+ * @see - https://react-hooks.abhushan.dev/hooks/window/usewindoweventlistener/
+ */
 export function useWindowEventListener(
 	eventName: keyof UseEventListenerEventMap<Window>,
 	callback: UseEventListenerCallback,

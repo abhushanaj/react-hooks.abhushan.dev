374590a9def83ce4987942f7eb78558641767f21
Abhushan A. Joshi <abhushanadhikarijoshi@gmail.com>
2024-03-08 14:28:54 +0530

    chore: add see link for docs

--- react-hooks/src/hooks/useFreshCallback/index.ts
+++ react-hooks/src/hooks/useFreshCallback/index.ts
@@ -3,9 +3,8 @@ import { useFreshRef } from '../useFreshRef';
 type CallbackWithArgs<T> = (...args: Array<T>) => void;
 
 /**
- * useFreshCallback() - Custom React hook to get the latest and fresh version of function with arguments
- * @param callback
- * @returns
+ * useFreshCallback() - Custom React hook to get the latest and fresh version of function with arguments.
+ * @see - https://react-hooks.abhushan.dev/hooks/utilities/usefreshcallback/
  */
 export function useFreshCallback<T>(callback: CallbackWithArgs<T>) {
 	const callbackRef = useFreshRef(callback);

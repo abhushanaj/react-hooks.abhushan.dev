d69c13ed5a918314289dd37d2474cab002f98840
Abhushan A. Joshi <abhushanadhikarijoshi@gmail.com>
2024-03-11 17:23:39 +0530

    feat: add test for immediat flag

--- react-hooks/src/hooks/useIntervalWhen/index.test.ts
+++ react-hooks/src/hooks/useIntervalWhen/index.test.ts
@@ -85,13 +85,12 @@ describe('useIntervalWhen() hook', () => {
 
 		it('with new duration (ms) passed during a re-render', () => {
 			expect.hasAssertions();
-			const cb = vi.fn();
 			const advancedFactor = 3;
 			const afterRerenderDelay = 500;
 
 			const { rerender } = renderHook(({ callback, duration }) => useIntervalWhen(callback, duration), {
 				initialProps: {
-					callback: cb,
+					callback: mockedCb,
 					duration: defaultDuration
 				}
 			});
@@ -104,11 +103,11 @@ describe('useIntervalWhen() hook', () => {
 			const expectedCalls = Math.floor((defaultDuration * advancedFactor) / defaultDuration);
 
 			// we expect the callback to be invoked 3 times
-			expect(cb).toHaveBeenCalledTimes(expectedCalls);
+			expect(mockedCb).toHaveBeenCalledTimes(expectedCalls);
 
 			// we change the delay to be 500ms
 			rerender({
-				callback: cb,
+				callback: mockedCb,
 				duration: afterRerenderDelay
 			});
 
@@ -119,7 +118,35 @@ describe('useIntervalWhen() hook', () => {
 
 			const newExpectedCalls = Math.floor((afterRerenderDelay * advancedFactor) / afterRerenderDelay);
 
-			expect(cb).toHaveBeenCalledTimes(newExpectedCalls + expectedCalls);
+			expect(mockedCb).toHaveBeenCalledTimes(newExpectedCalls + expectedCalls);
 		});
+	});
+
+	// usage with immediate flag
+	it('should invoke immediately and in durations (ms) specified', () => {
+		expect.hasAssertions();
+
+		expect.hasAssertions();
+
+		const advancedFactor = 3;
+
+		renderHook(({ callback, duration, immediate }) => useIntervalWhen(callback, duration, immediate), {
+			initialProps: {
+				callback: mockedCb,
+				duration: defaultDuration,
+				immediate: true
+			}
+		});
+
+		// as immediate flag is passed
+		expect(mockedCb).toHaveBeenCalledTimes(1);
+
+		// increase by 1000*3, so should be called 3 times
+		act(() => {
+			vi.advanceTimersByTime(defaultDuration * advancedFactor);
+		});
+
+		// we expect one call to happen immediately and then other calls after durations
+		expect(mockedCb).toHaveBeenCalledTimes(advancedFactor + 1);
 	});
 });

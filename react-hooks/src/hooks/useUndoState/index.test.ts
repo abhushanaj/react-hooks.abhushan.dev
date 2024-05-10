6f90e6cee4729ae110fb152bf5034291d59aadaf
Abhushan Adhikari Joshi <49617450+abhushanaj@users.noreply.github.com>
2024-05-10 12:55:48 +0530

    feat: useUndoState (#209)
    * feat: useUndoState hook added as an re-export of useStateWithHistory
    
    * feat: add docs for the useUndoState hook
    
    * feat: add missing reset action from useUndoState hook
    
    * chore: add changeset logs
    
    * chore: add missing assertion expectation assertion

---
+++ react-hooks/src/hooks/useUndoState/index.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from 'vitest';
+
+import { useUndoState } from '.';
+
+/**
+ * As this is a re-export of useStateWithHistory we don't need tests here. All tests for the parent hook should be enough.
+ */
+describe('useUndoState() hook', () => {
+	it('should be defined', () => {
+		expect.hasAssertions();
+		expect(useUndoState).toBeDefined();
+	});
+});
